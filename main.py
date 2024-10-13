2c220b186b343f0cbbb0125eb823228722d43ad0
Bulgaru Viorel <bulgaru.viorel.tudor@gmail.com>
2024-10-13 12:09:14 +0300

    Prima versiune de calculator prin prin care se valideaza orice numar intreg daca este prim.

--- main.py
+++
@@ -1,9 +0,0 @@
-from calculator import CalculatorPrim
-
-def main():
-    # Inițierea calculatorului de numere prime
-    calculator = CalculatorPrim()
-    calculator.run()
-
-if __name__ == "__main__":
-    main()
